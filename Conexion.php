5c842644f133f4988f1b586848102cfdedf2481b
Jorge <jemmgeorge@hotmail.com>
2021-11-10 10:52:34 -0500

    conectar base

---
+++ Conexion.php
@@ -0,0 +1,24 @@
+<?php
+
+class Conexion
+{
+
+    static public function Conectar() {
+        global $con;
+        $usuario = 'root';
+        $clave = '';
+        $con = new sqli('localhost',$usuario,$clave,'proyectofinal');
+        if ($con->connect_error)
+            echo "Fallo al conectar a MySQL: (" . $con->connect_errno . ") " . $con->connect_error;
+        else
+            return $con;
+    }
+    static public function Conectar_Usuario($usuario,$contrasena){
+        $con = new sqli("localhost",$usuario,$contrasena,"proyectofinal");
+        if ($con->connect_error)
+            echo "Fallo al conectar a MySQL: (" . $con->connect_errno . ") " . $con->connect_error;
+        else
+            return $con;
+    }
+}
+?>
