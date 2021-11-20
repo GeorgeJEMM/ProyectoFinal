ab1a6a374cef7c9b13c2dc63cde4e5330e51ec62
benalcazar77 <43661756+benalcazar77@users.noreply.github.com>
2021-11-20 10:42:52 -0500

    encabezado

--- plantilla/encabezado.php
+++ plantilla/encabezado.php
@@ -1,79 +1,13 @@
-<?php
-    include("./basededatos/procesos.php");
-?>
-<!doctype html>
-<html lang="es" style="">
+<!DOCTYPE html>
+<html lang="en">
 <head>
-
     <meta charset="UTF-8">
-    <meta name="description" content="HTML5">
-    <meta name="keyboards" content="HTML,CSS,JavaScript">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/x-icon" href="./css/Img/icono0.jpg" />
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
-    
-
-    <!-- Required meta tags -->
-    <link rel="stylesheet" href="./public/css/estilosCarrito.css">
-    <!--<link rel="stylesheet" href="./public/css/font-awesome-4.7.0/css/font-awesome.min.css">-->
-    <!-- Bootstrap CSS -->
-    <title>Compras</title>
-<!--  </style>
---></head>
-
-
-<div id="app" >
-    <header >
-        <div >
-            <nav class="navbar navbar-expand navbar-light bg-light">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="index.php">Inicio</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Nosotros</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Registro</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Categorías
-                                </a>
-                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                    <li><hr class="dropdown-divider"></li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                            </li>
-
-                        </ul>
-                        <div class="container justify-content-center">
-                            <a href="./vercarrito.php">
-                                <div class="circulo text-center"></div>
-                                <div class="carrito"></div>
-                            </a>
-                        </div>
-                        <form class="d-flex">
-                            <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
-                            <button class="btn btn-primary btn-lg" type="submit">Buscar</button>
-
-                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                <span class="navbar-toggler-icon"></span>
-                            </button>
-                            <a class="btn btn-primary btn-sm" href="bavanzada.php">Busqueda Avanzada</a>
-                        </form>
-                    </div>
-                </div>
-            </nav>
-        </div>
-    </header>
-</div>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        <?php if(isset($page_title)){ echo "$page_title"; } ?> Tienda Online
+    </title>
+    <link href="assets/css/bootstrap5.min.css" rel="stylesheet" >
+    <link href="assets/css/style.css" rel="stylesheet" >
+</head>
+<body>
